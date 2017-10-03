47d3216d0ea5ddc21b4030947d4afb4510dec19a
Jessie Yang <jessiejxyang@outlook.com>
2017-10-02 17:54:52 -0700

    (WIP) Create Bootstrap main page with menu functionality

---
+++ startbootstrap-simple-sidebar-gh-pages/js/main.js
@@ -0,0 +1,36 @@
+var app = angular.module('courseConnect', [
+  'ngRoute'
+]);
+
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/connect", {templateUrl: "partials/connect.html", controller: "PageCtrl"})
+    .when("/courses", {templateUrl: "partials/courses.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
