14d079a57512f4b083ae9cb6ad073bd6d457612e
Howe Cui <howe.cui@berkeley.edu>
2017-10-03 22:13:22 -0700

    revised backend structure

--- Facebook4J/Original Java Wrapper - Facebook4J/facebook4j-core/src/main/java/facebook4j/Reaction.java
+++
@@ -1,7 +0,0 @@
-package facebook4j;
-
-public interface Reaction extends IdNameEntity {
-
-    ReactionType getType();
-
-}
---
+++ backend/BackendProgram/src/Server.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Howe on 10/3/17.
+ */
+public class Server {
+
+
+
+    
+}
