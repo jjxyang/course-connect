6eeecfe9101bd1661c06264ddf04f813ada093d1
Howe Cui <howe.cui@berkeley.edu>
2017-10-04 08:42:35 -0700

    merging 10/3

--- backend/BackendProgram/src/Server.java
+++ backend/BackendProgram/src/Server.java
@@ -1,9 +1,21 @@
+import facebook4j.*;
+
 /**
  * Created by Howe on 10/3/17.
  */
 public class Server {
 
 
+    public static void main(String[] args){
 
-    
+        Facebook facebook = new FacebookFactory().getInstance();
+
+        Friendlist fList;
+
+    }
+
+
+
+
+
 }
