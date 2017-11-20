2e91e38ea2f633e05f83d64e67278844fa2ad9f2
Jessie Yang <jessiejxyang@outlook.com>
2017-11-20 04:38:48 -0800

    Remove extraneous code

--- web/js/client.js
+++ web/js/client.js
@@ -99,48 +99,10 @@ $(document).ready(function() {
       $loginPage.fadeOut();
       $joinPage.show();
       $loginPage.off('click');
-
-      // Tell the server your username
-      socket.emit('add user', username);
-    }
-  }
-
-  // Log a message
-  function log (message, options) {
-    var $el = $('<li>').addClass('log').text(message);
-    addMessageElement($el, options);
-  }
-
-  // Adds a message element to the messages and scrolls to the bottom
-  // el - The element to add as a message
-  // options.fade - If the element should fade-in (default = true)
-  // options.prepend - If the element should prepend
-  //   all other messages (default = false)
-  function addMessageElement (el, options) {
-    var $el = $(el);
-
-    // Setup default options
-    if (!options) {
-      options = {};
-    }
-    if (typeof options.fade === 'undefined') {
-      options.fade = true;
-    }
-    if (typeof options.prepend === 'undefined') {
-      options.prepend = false;
-    }
-
-    // Apply options
-    if (options.fade) {
-      $el.hide().fadeIn(FADE_TIME);
-    }
-    if (options.prepend) {
-      $messages.prepend($el);
     } else {
       alert("Sorry, you're not a Berkeley student!");
       console.log("you're not a berkeley student O:<");
     }
-    $messages[0].scrollTop = $messages[0].scrollHeight;
   }
 
   // Prevents input from having injected markup
@@ -160,18 +122,6 @@ $(document).ready(function() {
     return COLORS[index];
   }
 
-  // Socket events
-
-  // Whenever the server emits 'login', log the login message
-  // socket.on('login', function (data) {
-  //   connected = true;
-  //   // Display the welcome message
-  //   var message = "Welcome to Socket.IO Chat – ";
-  //   log(message, {
-  //     prepend: true
-  //   });
-  //   addParticipantsMessage(data);
-  // });
 
   // Whenever the server emits 'new message', update the chat body
   socket.on('new message', function (data) {
